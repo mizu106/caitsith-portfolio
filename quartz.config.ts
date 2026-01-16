e48328ef63164cee5dd4cd54ae27cfa8dbbf97f1
caitsith <mizu106@gmail.com>
2026-01-16 19:52:59 +0900

    CustomOgImagesJAをinportする場所をindes.tsに変更した。

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,6 +1,5 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
-import { CustomOgImagesJA } from "./quartz/plugins/CustomOgImages.ja"
 
 
 /**
--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -23,6 +23,7 @@ import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
+import CustomOgImagesJA from "./quartz/plugins/CustomOgImages.ja"
 
 export {
   ArticleTitle,
